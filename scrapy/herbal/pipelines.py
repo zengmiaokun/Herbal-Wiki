726de9f6963b41747e918a3eda2ee6b31552f2ae
Mango <zengmiaokun@live.com>
2019-05-18 21:42:58 +0800

    完善爬虫

--- scrapy/herbal/pipelines.py
+++ scrapy/herbal/pipelines.py
@@ -4,18 +4,33 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-# import pymysql
+
+
 import time
+import pymysql.cursors
 
 class HerbalPipeline(object):
+
+    def __init__(self):
+        # 连接数据库
+        self.connect = pymysql.connect(
+            host='',  # 数据库地址
+            port=3306,  # 数据库端口
+            db='herbal_wiki',  # 数据库名
+            user='',  # 数据库用户名
+            passwd='',  # 数据库密码
+            charset='utf8',  # 编码方式
+            use_unicode=True)
+        # 通过cursor执行增删查改
+        self.cursor = self.connect.cursor()
 
     def process_item(self, item, spider):
         log=open("error.log","a")
         try:
             if len(item) > 2:
-                for i in item:
-                    print(i + ":" + item[i])
-                print("\n")
+                self.cursor.execute("INSERT INTO herbal (%s) VALUES ('%s')" %(",".join(list(item.keys())),"','".join(list(item.values()))))
+                self.connect.commit()
+                return item
             else:
                 log.write(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())) + \
                 "\n" + item ["名称"]+ "内容为空，请检查异常原因。\n\n")
@@ -23,4 +38,7 @@ class HerbalPipeline(object):
         except TypeError:
             log.write(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())) + \
             "\n" + item ["名称"]+ "爬取异常，请检查错误原因。\n\n")
-        
+        except pymysql.err.InternalError:
+            for i in item:
+                print(i + ":" + item[i])
+                print("\n")
